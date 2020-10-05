031f7a6b3a8956f98bdc75f77bb9cbe0ce2b4552
Vincent <vp35@duke.edu>
2020-10-05 13:04:06 -0400

    wenxi's loader v0

--- pipeline/ingest/loaders/wenxi.py
+++ pipeline/ingest/loaders/wenxi.py
@@ -8,11 +8,11 @@ import h5py
 import ast
 import re
 
-from .jrclust import JRCLUST
+#from .jrclust import JRCLUST
 
 
 """
-This module houses a LoaderClass. Adjust it to conform to your data format
+This module houses a LoaderClass for Vincent's data format
     Input: 
         + root_data_dir: root directory of the data for all subjects
         + config: configuration parameters (typically, dj.config) 
@@ -104,11 +104,12 @@ Any loader class must have the following interfaces:
 """
 
 
-class TemplateLoader:
+class WenxiLoader:
 
-    tracking_camera = 'My_Camera'
+    tracking_camera = '' #TODO: specify here tracking device (to be added in tracking.py > TrackingDevice). Or pass that information to tracking_ingest function
     tracking_fps = 500
-    default_task = 'hf wheel'
+    default_task = 'hf wall dist' #TODO: describe task protocol used in experiment.py > Task > TaskProtocol
+    default_task_protocol = 100
 
     def __init__(self, root_data_dir, config={}):
         self.config = config
@@ -121,6 +122,8 @@ class TemplateLoader:
             raise FileNotFoundError(f'{subj_dir} not found!')
 
         # find all sessions' json files, which contain the processed files for each session
+        
+        """ # TODO: change this code block according to your file formats and folder structure
         all_sessions = list(subj_dir.rglob('**/*info.json'))
 
         # ---- parse processed data folders:
@@ -136,6 +139,7 @@ class TemplateLoader:
             # find all associated files
             session_files = [sess.relative_to(self.root_data_dir)
                              for sess in data_dir.glob(f'{sess_basename}*')]
+                             """
 
             yield {'subject_id': subject_name,
                    'session_date': sess_datetime.date(),
@@ -155,12 +159,15 @@ class TemplateLoader:
         #   PhotostimEvent  PhotostimTrial photostim_event_id  Photostim photostim_event_time power
         #   Project         project_name
 
+        """ # TODO: change this code block according to your file formats and folder structure
         # ---- get task type from the session file ----
         session_info_file = list(session_dir.glob(f'{session_basename}*.json'))
         with open(session_info_file[0]) as f:
             sessinfo = json.load(f)
         task = sessinfo.get('task', self.default_task) # if task not specified, set default (e.g, head-fixed wheel running)
         task_protocol = sessinfo.get('task_protocol', self.default_task_protocol) # same for task protocol
+
+        """
 
         # ---- get Photostim parameters (need to export notes first) ----
         photostim_params = sessinfo.get('photoStim')  #
@@ -181,11 +188,12 @@ class TemplateLoader:
                              'pulse_frequency': psp['stimFreq'],
                              'pulses_per_train': psp['trainLength'],
                              'waveform': psp.get('waveform', []),
-                             **psp.get('photostim_location', {})} 
+                             **psp.get('photostim_location', {})}  # TODO: get photostim_location from FO implant info
                 photostims.append(photostim)
 
         # ---- load files with TTLs and trial data ----
-        ephys_dir = session_dir / 'SpikeSorting' / session_basename 
+        """ # TODO: change this code block according to your file formats and folder structure
+        ephys_dir = session_dir / 'SpikeSorting' / session_basename # TODO: get TTLs from session directory, since some sessions do not have a spike sorting folder
         if not ephys_dir.exists():
             raise FileNotFoundError(f'{ephys_dir} not found!')
         ttl_file = os.path.join(ephys_dir, session_basename + '_TTLs.dat')
@@ -193,6 +201,7 @@ class TemplateLoader:
             with open(ttl_file, 'rb') as fid:
                 # load .dat with numpy. Load into 2 columns: .reshape((-1, 2)); to transpose: .T
                 ttl_ts = np.fromfile(fid, np.single)
+        """
 
         # ---- get trial info ----
         # (can be found in session's json file, or read from trial.csv. First solution is the most straightforward)
@@ -234,6 +243,7 @@ class TemplateLoader:
     def load_tracking(self, session_dir, subject_name, session_basename):
 
         # ---- identify the .mat file for tracking data ----
+        """ # TODO: change this code block according to your file formats and folder structure
         tracking_dir = session_dir / 'WhiskerTracking'
         if not tracking_dir.exists():
             raise FileNotFoundError(f'{tracking_dir} not found!')
@@ -249,6 +259,7 @@ class TemplateLoader:
 
         # ---- load .mat and extract whisker data ----
         trk_mat = spio.loadmat(tracking_fp, struct_as_record=False, squeeze_me=True)[tracking_fp.stem]
+        """
 
         frames = np.arange(max(trk_mat.fid) + 1)  # frame number with respect to tracking
 
@@ -264,6 +275,9 @@ class TemplateLoader:
                 d[matched_frame_idx] = getattr(trk_mat, var)[matched_wid]
                 whiskers[wid][var] = d
 
+        """ # TODO: write code to load wall and other tracking data
+        """
+
         # ---- Time sync ----
         # TODO: any time synchronization here
         # ---- return ----
@@ -276,6 +290,7 @@ class TemplateLoader:
                  'WhiskerTracking': [{'whisker_idx': wid, **wdata} for wid, wdata in whiskers.items()]}]
 
     def load_ephys(self, session_dir, subject_name, session_basename):
+        """ # TODO: change this code block according to your file formats and folder structure
         spikesorting_dir = session_dir / 'SpikeSorting' / f'{session_basename}'
         if not spikesorting_dir.exists():
             raise FileNotFoundError(f'{spikesorting_dir} not found!')
@@ -319,7 +334,9 @@ class TemplateLoader:
                     probe_params[k.strip()] = v.strip()
 
         channel_map = np.array(probe_params['channels'])
+        """
 
+        """ # TODO: change this code block to load data from your spike sorting output 
         # read JRCLUST results
         jrclust = JRCLUST(jrclust_fp[0])
 
@@ -328,6 +345,7 @@ class TemplateLoader:
         probe_comment = sessinfo['ephys']['probe']
         adapter = sessinfo['ephys']['adapter']
         headstage = rec_info['recording_system'] + '_' + adapter[adapter.find('OM') + 2:]
+        """
 
         probe_data = {'probe_comment': probe_comment,
                       'adapter': adapter,
